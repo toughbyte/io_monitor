ffac46efc996fc6261b2f31c1c0f9d1a3ed3a6ba
Dmitry Matveyev <public@greenfork.me>
2023-07-26 15:29:14 +0600

    Fix nil response in payload

--- lib/io_monitor/controller.rb
+++ lib/io_monitor/controller.rb
@@ -37,7 +37,7 @@ module IoMonitor
         data[source] = aggregator.get(source)
       end
 
-      data[:response] = payload[:response].body.bytesize
+      data[:response] = payload[:response].body.bytesize if payload.key?(:response)
     end
 
     private
